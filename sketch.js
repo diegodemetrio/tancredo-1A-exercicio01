5e9ec3ebfaac80ed98a2c3e89bdde1ccc2dff2f7
Demetrio <142011773+diegodemetrio@users.noreply.github.com>
2023-08-11 08:18:29 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+  createCanvas(400, 400);
+  background("silver");
+}
+
+function draw() {
+  
+ stroke("purple"); 
+  
+fill('gold'); 
+  ;
+if(mouseIsPressed){
+  rect(mouseX,mouseY,30,30)
+  
+}
+}
